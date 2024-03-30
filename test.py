87f04ab54a138eb1e514cbdb7f7974af58b9e27a
Nihal T M <nihaltmdev@gmail.com>
2024-03-30 17:35:53 +0530

    Removed tests for type errors and index endpoint as they make no sense when having our own client

--- test.py
+++ test.py
@@ -21,11 +21,6 @@ class Test_App(unittest.TestCase):
         self.client = client(host="localhost", port=2379)
         self.client.put("test_key", "test_value")
 
-    # def test_index(self):
-    #     response = self.app.get("/")
-    #     data = json.loads(response.data.decode())
-    #     self.assertEqual(data["message"], "Welcome to the demo server!")
-
     def test_put_correct(self):
         data = {"key": "test_key", "value": "test_val"}
         response = self.app.post("/put", json=data)
@@ -39,21 +34,14 @@ class Test_App(unittest.TestCase):
         response = self.app.post("/put", json=data)
         data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(data["message"], "Key and Value both needs to be provided!")
+        self.assertEqual(data["message"], "Key and Value both need to be provided!")
         
         # Missing Key
         data = {"key": "", "value": "test_missing"}
         response = self.app.post("/put", json=data)
         data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(data["message"], "Key and Value both needs to be provided!")
-
-    # def test_put_typeerror(self):
-    #     data = {"key": 3, "value": 10}
-    #     response = self.app.post("/put", json=data)
-    #     data = json.loads(response.data.decode())
-    #     self.assertEqual(response.status_code, 400)
-    #     self.assertEqual(data["message"], "Key and Values to be inserted have to be string type!")
+        self.assertEqual(data["message"], "Key and Value both need to be provided!")
 
     def test_get_missing_key(self):
         data = {"key": ""}
@@ -61,15 +49,6 @@ class Test_App(unittest.TestCase):
         data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 400)
         self.assertEqual(data["message"], "The key has to be provided!")
-
-    # def test_get_typeerror(self):
-    #     self.client.put("test_key", "test_value")
-
-    #     data = {"key": 3}
-    #     response = self.app.post("/get", json=data)
-    #     data = json.loads(response.data.decode())
-    #     self.assertEqual(response.status_code, 400)
-    #     self.assertEqual(data["message"], "Key should be of string type!")
 
     def test_get_nonexistent_key(self):
         data = {"key": "non-existent"}
@@ -96,7 +75,6 @@ class Test_App(unittest.TestCase):
         self.assertEqual(resp_keys, key_arr)
         self.assertEqual(resp_vals, val_arr)
 
-
     def test_delete_missing_key(self):
         self.client.put("test_key", "test_value")
 
@@ -105,15 +83,6 @@ class Test_App(unittest.TestCase):
         data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 400)
         self.assertEqual(data["message"], "The key has to be provided!")
-
-    # def test_delete_typeerror(self):
-    #     self.client.put("test_key", "test_value")
-
-    #     data = {"key": 3}
-    #     response = self.app.delete("/delete", json=data)
-    #     data = json.loads(response.data.decode())
-    #     self.assertEqual(response.status_code, 400)
-    #     self.assertEqual(data["message"], "Key should be of string type!")
 
     def test_delete_nonexistent_key(self):
         data = {"key": "non-existent"}
@@ -174,8 +143,7 @@ class Test_App(unittest.TestCase):
         data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data["message"], "Key 'test_key' deleted successfully!")
-
-        
+      
 if __name__ == "__main__":
     suite = unittest.TestLoader().loadTestsFromTestCase(Test_App)
     unittest.TextTestRunner(resultclass=CustomTestResult, verbosity=2).run(suite)
